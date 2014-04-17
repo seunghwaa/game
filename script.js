0e115f62bafb7f6d2b1e7a3258a2b9b63996f54e
seunghwaa <hwa9272@gmail.com>
2014-04-18 01:19:22 +0900

    수정

--- script.js
+++ script.js
@@ -1,60 +1,57 @@
 $(document).ready(function(){
+
 	$('#c1').click(function(){
 		$(this).toggleClass('backF');
 	});
 
+	$('#c2').click(function(){
+    		$(this).toggleClass('backE');
+  	});
+  
+	$('#c3').click(function(){
+    		$(this).toggleClass('backA');
+  	}); 
+  
+	$('#c4').click(function(){
+   		 $(this).toggleClass('backF');
+ 	 });  
+  
+	$('#c5').click(function(){
+    		$(this).toggleClass('backE');
+  	});
+  
+	$('#c6').click(function(){
+   		 $(this).toggleClass('backA');
+  	});
+  
+	$('#c7').click(function(){
+    		$(this).toggleClass('backB');
+  	});
+  
+	$('#c8').click(function(){
+    		$(this).toggleClass('backD');
+  	});
+  
+	$('#c9').click(function(){
+    		$(this).toggleClass('backB');
+  	});  
+  
+	$('#c10').click(function(){
+    		$(this).toggleClass('backC');
+ 	});  
+  
+	$('#c11').click(function(){
+   		 $(this).toggleClass('backD');
+  	});  
+  
+	$('#c12').click(function(){
+    		$(this).toggleClass('backC');
+  	});  
 
-
-
-//$(document).ready(function() {
-  //$('#c1').click(function(){
-    //$(this).toggleClass('backF');
-  //});
-
-  $('#c2').click(function(){
-    $(this).toggleClass('backE');
-  });
-  
-$('#c3').click(function(){
-    $(this).toggleClass('backA');
-  }); 
-  
-$('#c4').click(function(){
-    $(this).toggleClass('backF');
-  });  
-  
-$('#c5').click(function(){
-    $(this).toggleClass('backE');
-  });
-  
-$('#c6').click(function(){
-    $(this).toggleClass('backA');
-  });
-  
-$('#c7').click(function(){
-    $(this).toggleClass('backB');
-  });
-  
-$('#c8').click(function(){
-    $(this).toggleClass('backD');
-  });
-  
-$('#c9').click(function(){
-    $(this).toggleClass('backB');
-  });  
-  
-$('#c10').click(function(){
-    $(this).toggleClass('backC');
-  });  
-  
-$('#c11').click(function(){
-    $(this).toggleClass('backD');
-  });  
-  
-$('#c12').click(function(){
-    $(this).toggleClass('backC');
-  });    
 });
+
+
+
 
   
 
